9e799e6224a1ed77e2c0b6f865f3041242eb4f82
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-31 13:14:06 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/actions/server/routes/connector/check_id/check_id.ts
+++ x-pack/platform/plugins/shared/actions/server/routes/connector/check_id/check_id.ts
@@ -7,7 +7,6 @@
 
 import type { IRouter } from '@kbn/core/server';
 import type { ILicenseState } from '../../../lib';
-import { BASE_ACTION_API_PATH } from '../../../../common';
 import type { ActionsRequestHandlerContext } from '../../../types';
 import { verifyAccessAndContext } from '../../verify_access_and_context';
 import { DEFAULT_ACTION_ROUTE_SECURITY } from '../../constants';
